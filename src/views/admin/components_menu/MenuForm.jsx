eccb078e4e08b8d974bfa4597f94ea8691b1dc7c
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2022-09-20 18:21:09 +0800

    adding media querries

--- src/views/admin/components_menu/MenuForm.jsx
+++ src/views/admin/components_menu/MenuForm.jsx
@@ -2,7 +2,7 @@
 import { CheckOutlined, LoadingOutlined } from '@ant-design/icons';
 import { ImageLoader } from 'components/common';
 import {
-  CustomColorInput, CustomCreatableSelect, CustomInput, CustomTextarea
+  CustomColorInput, CustomCreatableSelect, CustomInput, CustomTextarea, CustomMobileInput
 } from 'components/formik';
 import {
   Field, FieldArray, Form, Formik
@@ -17,6 +17,7 @@ import { OrderItem } from 'components/basket';
 import { useSelector } from 'react-redux';
 
 import { displayActionMessage, displayDate, displayMoney } from 'helpers/utils';
+
 
 
 
@@ -432,11 +433,81 @@ const MenuForm = ({ menu, onSubmit, isLoading, basket }) => {
 
 
 
+
+
+
+            <div className="user-profile-details">
+              <Field
+                disabled={isLoading}
+                name="fullname"
+                type="text"
+                label="* Full Name"
+                placeholder="Enter your full name"
+                component={CustomInput}
+                style={{ textTransform: 'capitalize' }}
+              />
+              {/* <Field
+                disabled={authProvider !== 'password' || isLoading}
+                name="email"
+                type="email"
+                label="* Email Address"
+                placeholder="test@example.com"
+                component={CustomInput}
+              /> */}
+              <Field
+                disabled={isLoading}
+                name="address"
+                type="text"
+                label="Address (Will be used for checkout)"
+                placeholder="#245 Brgy. Maligalig, Arayat Pampanga, Philippines"
+                component={CustomInput}
+                style={{ textTransform: 'capitalize' }}
+              />
+              <CustomMobileInput
+                defaultValue={values.mobile}
+                name="mobile"
+                disabled={isLoading}
+                label="Mobile Number (Will be used for checkout)"
+              />
+              <br />
+              <div className="edit-user-action">
+                <button
+                  className="button button-muted w-100-mobile"
+                  disabled={isLoading}
+                  onClick={() => history.push(ACCOUNT)}
+                  type="button"
+                >
+                  {/* <ArrowLeftOutlined /> */}
+                  &nbsp;
+                  Back to Profile
+                </button>
+                {/* <button
+                  className="button w-100-mobile"
+                  disabled={isLoading}
+                  onClick={submitForm}
+                  type="button"
+                >
+                  {isLoading ? <LoadingOutlined /> : <CheckOutlined />}
+                  &nbsp;
+                  {isLoading ? 'Updating Profile' : 'Update Profile'}
+                </button> */}
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
             <div className="order">
 
 
               <button
-                className="button button-muted w-100-mobile"
+                className="button-back button-muted w-100-mobile"
                 disabled={isLoading}
                 onClick={() => history.push(ADMIN_MENUS)}
                 type="button"
