3f058b66758fa0eb92e1020126dd23b92e42ee65
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2022-11-02 17:36:12 +0800

    sidebar final adjustemnts

--- src/components/common/Side.jsx
+++ src/components/common/Side.jsx
@@ -5,10 +5,22 @@ import { Icon } from '../common/Icon';
 
 import "../../styles/side/index.css"
 
+import { useState } from "react";
+
+
+// Alternative
+
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
+
 
 
 
 import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+
+// React Sidebar Pro Yellow Version
+
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+
 
 
 
@@ -17,7 +29,24 @@ const Side = () => {
 
 
 
+
+
+  // React Sidebar Pro
+
+
   const { collapseSidebar } = useProSidebar();
+
+
+
+
+  // Alternative
+
+  const [favorite, setFavorite] = useState(false);
+
+  const toggleFavorite = () => setFavorite((prev) => !prev);
+
+
+
 
 
 
@@ -58,14 +87,56 @@ const Side = () => {
 
             <MenuItem icon={<Icon name="book-2" />}>Documentation1</MenuItem>
 
+            {/* clsoe part */}
+
+            <MenuItem
+              icon={<Icon name="close" />}
+              onClick={() => collapseSidebar()}
+            >
+              Close
+            </MenuItem>
+
+
+
+            {/* <button onClick={() => collapseSidebar()}>Collap1</button> */}
+
+
+
+
+
+            {/* <div onClick={menuIconClick}> */}
+
+            {/* changing menu collapse icon on click */}
+
+            {/* {collapseSidebar ? (
+                <FiArrowRightCircle />
+              ) : (
+                <FiArrowLeftCircle />
+              )}
+            </div> */}
+
+
+
+            <button
+              // onClick={toggleFavorite} 
+              onClick={() => { toggleFavorite(); collapseSidebar() }}
+              className="top-rated-car-react-button">
+              {favorite ? (
+                <MdFavoriteBorder style={{ color: "#F76631" }} />
+              ) : (
+                <MdFavorite style={{ color: "#F76631" }} />
+              )}
+            </button>
+
+
 
 
 
           </Menu>
         </Sidebar>
-        <main>
+        {/* <main>
           <button onClick={() => collapseSidebar()}>Collap1</button>
-        </main>
+        </main> */}
       </div>
 
 
