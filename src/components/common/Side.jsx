5efee20a907601c1014f89c14cad296a0f73c5ae
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2022-11-03 20:20:25 +0800

    sidebar adjustments width/padding

--- src/components/common/Side.jsx
+++ src/components/common/Side.jsx
@@ -1,25 +1,23 @@
-import { ADMIN_DASHBOARD } from 'constants/routes';
-import React from 'react';
-
+import React, { useState } from 'react';
 import { Icon } from '../common/Icon';
-
 import "../../styles/side/index.css"
-
-import { useState } from "react";
-
 
 // Alternative
 
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
 
-
-
-import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, useProSidebar, collapseSidebar } from 'react-pro-sidebar';
 
 // React Sidebar Pro Yellow Version
 
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle, FiSettings } from "react-icons/fi";
+
+import { ADMIN_PRODUCTS, ADMIN_ORDERS, ADMIN_POSTS, ADMIN_USERS1, ADMIN_SETTINGS, ADMIN_DASHBOARD } from 'constants/routes';
+
+import { NavLink } from 'react-router-dom';
+
+
 
 
 
@@ -59,75 +57,117 @@ const Side = () => {
 
 
       <div style={{ display: 'flex', height: '100%' }}>
-        <Sidebar>
+
+        <Sidebar
+          style={{ minWidth: '60px', collapsedWidth: '60px' }}
+
+        >
           <Menu>
 
+
+
+            <NavLink
+             to={ADMIN_DASHBOARD}>
+              <MenuItem
+
+                icon={<Icon name="settings" />}
+              >
+                Settings
+
+              </MenuItem>
+            </NavLink>
+
+
+            <NavLink
+             to={ADMIN_POSTS}>
+
             <MenuItem
-            // class="bi bi-file-earmark-richtext"
+
+              icon={<Icon name="posts" />}
             >
-              Documentation1
+              Posts
+
             </MenuItem>
 
+            </NavLink>
+
+
             <MenuItem
-            // class="bi bi-calendar"
+
+              icon={<Icon name="users" />}
             >
-              Calendar
+              Users
+
             </MenuItem>
 
             <MenuItem
 
+              icon={<Icon name="products" />}
             >
-              E-commerce
+              Products
+
             </MenuItem>
 
             <MenuItem
 
-            >Documentation
+              icon={<Icon name="orders" />}
+            >
+              Orders
+
             </MenuItem>
+
+
+
+
+
+
+
+
+
 
             <MenuItem icon={<Icon name="book-2" />}>Documentation1</MenuItem>
 
-            {/* clsoe part */}
 
-            <MenuItem
-              icon={<Icon name="close" />}
-              onClick={() => collapseSidebar()}
-            >
-              Close
+
+
+
+
+
+
+
+            <MenuItem >
+              {/* <span> */}
+
+              <button
+                // onClick={toggleFavorite} 
+                onClick={() => { toggleFavorite(); collapseSidebar() }}
+                className="top-rated-car-react-button">
+                {favorite ? (
+                  <FiArrowRightCircle
+                    // className='bigger' 
+                    style={{ color: "#F76631", width: "24px", height: "24px" }} />
+
+                ) : (
+                  <FiArrowLeftCircle
+                    // className='bigger' 
+                    style={{ color: "#F76631", width: "24px", height: "24px" }} />
+                )}
+              </button>
+              {/* </span> */}
             </MenuItem>
 
 
 
-            {/* <button onClick={() => collapseSidebar()}>Collap1</button> */}
-
-
-
-
-
-            {/* <div onClick={menuIconClick}> */}
-
-            {/* changing menu collapse icon on click */}
-
-            {/* {collapseSidebar ? (
-                <FiArrowRightCircle />
-              ) : (
-                <FiArrowLeftCircle />
-              )}
-            </div> */}
-
-
-
-            <button
+            {/* <button
               // onClick={toggleFavorite} 
               onClick={() => { toggleFavorite(); collapseSidebar() }}
               className="top-rated-car-react-button">
               {favorite ? (
-                <MdFavoriteBorder style={{ color: "#F76631" }} />
+                <FiArrowRightCircle style={{ color: "#F76631" }} />
               ) : (
-                <MdFavorite style={{ color: "#F76631" }} />
+                <FiArrowLeftCircle style={{ color: "#F76631" }} />
               )}
-            </button>
-
+            </button> */}
 
 
 
