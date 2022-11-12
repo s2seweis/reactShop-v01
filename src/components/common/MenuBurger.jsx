d671f919c09235428fbd9ecf955528bd3684f8e8
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2022-11-12 11:41:33 +0800

    burger menu adjustments

--- src/components/common/MenuBurger.jsx
+++ src/components/common/MenuBurger.jsx
@@ -3,6 +3,13 @@ import { slide as Menu } from "react-burger-menu";
 import { Link, useLocation } from 'react-router-dom';
 
 import logo from 'images/logo-full.png';
+import dignlogo from '../../../static/dign-logo.png';
+
+import { Icon } from '../common/Icon';
+
+
+import { ADMIN_PRODUCTS, ADMIN_ORDERS, ADMIN_POSTS, ADMIN_USERS1, ADMIN_SETTINGS, ADMIN_DASHBOARD, SHOP } from 'constants/routes';
+
 
 
 
@@ -26,14 +33,24 @@ const MenuBurger = () => {
 
       <div className="logo">
         <Link
-          // onClick={onClickLink} to="/"
-          >
-          <img alt="Logo" src={logo} />
-          </Link>
+        // onClick={onClickLink} to="/"
+        >
+          <img alt="Logo" src={dignlogo}
+            style={{ width: '100%', borderTop: "none" }}
+          />
+        </Link>
       </div>
 
 
-      <a className="menu-item" href="/">
+
+
+
+
+
+      <a className="menu-item" href="/"
+        active={window.location.pathname === "/"}
+
+      >
         Home
       </a>
 
