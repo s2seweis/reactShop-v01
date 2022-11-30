3f3b395e5d7dbb4f13306da992e92eaceb5870c2
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2022-11-30 17:49:41 +0800

    cleaning of the code

--- src/views/view_product/index.jsx
+++ src/views/view_product/index.jsx
@@ -16,22 +16,20 @@ import { Link, useParams } from 'react-router-dom';
 import Select from 'react-select';
 
 
-import { useSelector } from 'react-redux';
 
-import { useTranslation } from 'react-i18next'
 
 import Ingredients from 'components/common/Ingredients';
 
-import $ from 'jquery';
-
-
-const defaultSelectValue = "Select a fruit"
+// import $ from 'jquery';
 
 
 
 
 
-const ViewProduct = (fruitDetector) => {
+
+
+
+const ViewProduct = () => {
 
 
 
@@ -39,7 +37,6 @@ const ViewProduct = (fruitDetector) => {
   const { id } = useParams();
   const { product, isLoading, error } = useProduct(id);
   const { addToBasket, isItemOnBasket } = useBasket(id);
-  // console.log(product)
 
   useScrollTop();
   useDocumentTitle(`View ${product?.name || 'Item'}`);
@@ -48,16 +45,9 @@ const ViewProduct = (fruitDetector) => {
 
   const [selectedSize, setSelectedSize] = useState('');
 
-  // Addind selected Price for Basket
   const [selectedPrice, setSelectedPrice] = useState('');
 
-  // =>
-
-  // making it to option and to setOption
-
-
-
-  // console.log(selectedPrice)
+  
 
 
   const [selectedColor, setSelectedColor] = useState('');
@@ -85,8 +75,6 @@ const ViewProduct = (fruitDetector) => {
 
 
 
-  // Test:1 onSelectedSizeChange almost the same like handleChange
-
   const onSelectedSizeChange = (newValue) => {
     setSelectedSize(newValue.value);
     // console.log(newValue.value)
@@ -100,19 +88,10 @@ const ViewProduct = (fruitDetector) => {
 
 
 
-  // Test:1 -----end
-
-
-
   const [dropValue, setDropValue] = useState();
 
   const [option, setOption] = useState();
-
-
-
   // console.log(option)
-
-
 
 
 
@@ -140,220 +119,17 @@ const ViewProduct = (fruitDetector) => {
   };
 
 
+ 
 
+  {/* Test ----------------Start */ }
+  // https://javascript.plainenglish.io/how-to-retrieve-the-text-of-the-selected-option-element-in-a-select-element-with-javascript-6933e5d4457d
 
-  // const [size, setSize] = React.useState();
-  // const [price, setPrice] = React.useState();
-  // console.log(price)
 
+  // its working?
 
 
-  // Test ----------------
 
-
-
-
-  // const myFunction = () => {
-  //   var x = document.getElementById("mySelect").options[0].text;
-  //   document.getElementById("demo").innerHTML = x;
-  //   console.log(x)
-
-  // }
-
-
-  // const myFunction = () => {
-  //   var x = document.getElementById("mySelect");
-  //   var i = x.selectedIndex;
-  //   document.getElementById("demo").innerHTML = x.options[i].text;
-  //   console.log(x)
-
-  // }
-
-
-  // let log = document.getElementsById('test').selectedValue;
-  // console.log('log')
-
-
-  let log = document.getElementById('log');
-  // console.log('log')
-
-
-
-  // const value = React.useRef('');
-  // console.log(value)
-
-
-
-
-  const handleChange1 = (e) => {
-    let innerText = e.target[e.target.options.selectedIndex].innerText;
-    let value = e.target.value;
-    // console.log(innerText)
-    // console.log(value)
-
-
-
-    log.innerHTML = `<table>
-      <tr><th>value</th><th>innerText</th></tr>
-      <tr><td>${value}</td><td>${innerText}</td></tr>
-    </table>`;
-
-
-    // console.log(innerText)
-    // console.log(value)
-
-
-
-  }
-
-
-
-  // const ref = React.useRef();
-  // log on second render
-  // My name is Shubham, I work for  for the last 5 years.
-  // console.log(ref.current?.innerText);
-
-
-
-  // Test ----------------End
-
-
-  const Welcome = (props) => {
-    return <div>
-      <p>Hello {props.name}!</p>
-      {props.children
-      }
-
-    </div>
-  };
-
-
-  // Test ----------------End
-
-
-
-  // export defaWelcomelt function PaperLetter(props) {
-  //   const [clicked, setclicked] = useState(false);
-  //   const letterRef = useRef();
-
-
-
-
-
-  // const textInput = useRef(null);
-
-
-  // const size = useRef(null);
-
-  // const size = React.useRef();
-  // const ref = React.useRef();
-
-
-
-  // const size = React.useRef();
-
-
-
-
-
-  // Test ----------------End
-
-
-  const [selected, setSelected] = useState(defaultSelectValue)
-  // console.log(selected);
-
-
-
-
-  // Test ----------------End
-
-
-
-
-  // var e = document.getElementById("ddlViewBy");
-
-  // function onChange() {
-  //   var value = e.value;
-  //   var text = e.options[e.selectedIndex].text;
-  //   console.log(value, text);
-  // }
-
-  // e.onchange = onChange;
-  // onChange();
-
-
-
-
-
-
-
-  // Test ----------------End
-
-
-
-
-
-
-
-  var e = document.getElementById("ddlViewBy");
-  // var e = document.getElementById("elementId");
-
-
-
-  const handleChange2 = () => {
-    // var value = e.value;
-    // var text = e.options[e.selectedIndex].text;
-
-
-
-    var value = e.value;
-    var text = e.options[e.selectedIndex].text;
-    console.log(value);
-    console.log(text);
-
-
-
-
-  }
-
-
-
-  {/* Test ----------------End */ }
-
-
-
-
-
-
-  const [inputValue, setInputValue] = useState("");
-  // console.log(inputValue);
-
-  const count = useRef(0);
-  // console.log(count);
-
-
-
-
-
-
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
-
-
-
-
-
-
-  const ref = React.useRef();
-
-
-  
-
-
-  {/* Test ----------------End */ }
-
-   const getSelectedText = (el) => {
+  const getSelectedText = (el) => {
     if (el?.selectedIndex === -1) {
       return null;
     }
@@ -364,6 +140,8 @@ const ViewProduct = (fruitDetector) => {
   const select = document.querySelector('select')
   const text = getSelectedText(select);
   console.log(text)
+
+  {/* Test ----------------End */ }
 
 
 
@@ -409,14 +187,14 @@ const ViewProduct = (fruitDetector) => {
 
 
 
-            {/* <div className="product-modal-image-wrapper">
+            <div className="product-modal-image-wrapper">
               {selectedColor && <input type="color" disabled ref={colorOverlay} id="color-overlay" />}
               <ImageLoader
                 alt={product.name}
                 className="product-modal-image"
                 src={selectedImage}
               />
-            </div> */}
+            </div>
 
 
 
@@ -455,10 +233,10 @@ const ViewProduct = (fruitDetector) => {
 
 
 
+              {/* Test: --------------------Start */}
 
 
-
-              <div>
+              {/* <div>
                 <div className="dropdown">
 
 
@@ -474,7 +252,10 @@ const ViewProduct = (fruitDetector) => {
                 <button className="button-variant" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                   {dropValue == null || dropValue == '' ? 'Pick a Size' : dropValue}
                 </button>
-              </div>
+              </div> */}
+
+
+              {/* Test: --------------------Start */}
 
 
 
@@ -488,13 +269,13 @@ const ViewProduct = (fruitDetector) => {
                 <option value={0}  >Pick Your Size</option>
                 <option value={product?.sizes_prices.small}>
 
-                
-
-                    small
 
 
+                  small
 
-                
+
+
+
 
                 </option>
 
@@ -504,34 +285,11 @@ const ViewProduct = (fruitDetector) => {
               </select>
 
 
-              {/* <p id="demo"></p> */}
+
 
               <button className="button-variant" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 {option == null || option == '' ? 'Pick!!' : option}
               </button>
-
-
-
-
-
-
-
-              <div className="vari">
-                <select id="greet" onChange={handleChange1}>
-                  <option value={product?.sizes_prices.small}>{product?.sizes_new.small} (innerText)</option>
-                  <option value="value_goodbye">Goodbye innerText</option>
-                  <option value="value_seeYou">See you... innerText</option>
-                </select>
-
-
-                <div id="log"></div>
-
-
-
-
-              </div>
-
-
 
 
 
@@ -558,23 +316,6 @@ const ViewProduct = (fruitDetector) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
               <div className="product-modal-action">
                 <button
                   className={`button button-small ${isItemOnBasket(product.id) ? 'button-border button-border-gray' : ''}`}
@@ -584,192 +325,6 @@ const ViewProduct = (fruitDetector) => {
                   {isItemOnBasket(product.id) ? 'Remove From Basket' : 'Add To Basket'}
                 </button>
               </div>
-
-
-
-
-
-              {/* Test w3_test------------------- */}
-
-              {/* <div className="w3_test">
-                <form>
-                  <select id="mySelect">
-                    <option>Apple</option>
-                    <option>Orange</option>
-                    <option>Pineapple</option>
-                    <option>Banana</option>
-                  </select>
-                </form>
-
-                <p className="w3_test">Click the button to display the text of the first option in the drop-down list.</p>
-
-                <button onClick={myFunction}>Try it</button>
-
-                <p id="demo"
-                  className="w3_test"></p>
-
-              </div> */}
-
-
-              {/* Test ----------------End */}
-
-
-
-              {/* <div className="w3_test">
-                <form>
-                  <select id="mySelect" onChange={myFunction}>
-                    <option>Apple</option>
-                    <option>Orange</option>
-                    <option>Pineapple</option>
-                    <option>Banana</option>
-                  </select>
-                </form>
-
-                <p id="demo"></p>
-
-              </div> */}
-
-
-
-
-
-              <div className='w3_test'>
-
-                <Welcome name="Sara">good day </Welcome>
-
-
-
-                {/* Atempt:10 */}
-
-                {/* <div ref={ref}>
-                My name is Shubham, I work for <input type="text" /> for the last 5 years.
-              </div> */}
-
-              </div>
-
-
-
-
-              {/* <select id="test">
-                <option value="1">Test One</option>
-                <option value="2">Test Two</option>
-              </select> */}
-
-
-
-
-
-              {/* Test ----------------End */}
-
-
-
-
-
-
-
-
-              <div className='w3_test'>
-
-                <>
-                  <label htmlFor="fruits">Fruits</label>{' '}
-                  <select
-                    id="fruits"
-                    name="fruits"
-                    defaultValue={selected}
-                    style={{ color: selected === defaultSelectValue ? "gray" : "black" }}
-                    // onClick={() => { toggleFavorite(); collapseSidebar() }}
-
-                    onChange={e => { setSelected(e.target.value) }}
-                  >
-                    <option>{defaultSelectValue}</option>
-                    <option
-                    >
-                      Banana
-                    </option>
-
-                    <option>Apple</option>
-                    <option>Orange</option>
-                  </select>
-
-                  <h2>Selected: {selected}</h2>
-                </>
-
-              </div>
-
-
-
-
-
-
-
-              {/* Test ----------------End */}
-
-
-
-
-              {/* <form>
-                <select id="ddlViewBy">
-                  <option value="1">test1</option>
-                  <option value="2" selected="selected">test2</option>
-                  <option value="3">test3</option>
-                </select>
-              </form> */}
-
-
-
-
-
-
-
-              {/* Test ----------------End */}
-
-              <div className='w3_test'>
-                <form>
-                  <select id="ddlViewBy"
-                    onChange={handleChange2}
-                  >
-                    <option value="1">test1</option>
-                    <option value="2" selected="selected">test2</option>
-                    <option value="3">test3</option>
-                  </select>
-                </form>
-              </div>
-
-              {/* Test ----------------End */}
-
-
-
-
-
-              <div className='w3_test'>
-                <>
-                  <input
-                    type="text"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                  />
-                  <h1>Render Count: {count.current}</h1>
-                </>
-              </div>
-
-
-              {/* Test ----------------End */}
-
-
-
-
-              {/* <div className='w3_test' ref={ref}>
-
-                small
-
-
-
-              </div> */}
-
-
-
-
-              {/* Test ----------------End */}
 
 
 
