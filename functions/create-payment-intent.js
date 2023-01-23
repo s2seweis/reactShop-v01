ca3418d667b60c85e2f92bab3a958c6f792738d2
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-01-23 15:01:38 +0800

    v42

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+
+const stripe = require("stripe")("sk_test_51MSB2wIlCEHks7DgwfqyPzQNTkKjQiu40vmGeMLba5IJwiX3GclG65xYKU2FEcFAoyJjCO6cQyLQWs9mtERKbriz00i2JtPPK0")
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+// export async function handler(event) {
+export async function handler(event) {
+
+  try {
+
+    const { amount } = JSON.parse(event.body);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"]
+
+    });
+
+    return {
+
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent })
+
+    }
+
+  } catch (error) {
+
+    console.log({ error });
+
+    return {
+
+      status: 400,
+      body: JSON.stringify( { error } )
+
+    }
+
+
+  }
+
+
+
+}
