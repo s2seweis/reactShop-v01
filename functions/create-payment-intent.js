29573faaf80d05e488ec1672b6a10f614218d92a
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-01-23 16:48:22 +0800

    v98

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 // test
 
-// const stripe = require("stripe")("sk_test_51MSB2wIlCEHks7DgwfqyPzQNTkKjQiu40vmGeMLba5IJwiX3GclG65xYKU2FEcFAoyJjCO6cQyLQWs9mtERKbriz00i2JtPPK0")
+const stripe = require("stripe")("sk_test_51MSB2wIlCEHks7DgwfqyPzQNTkKjQiu40vmGeMLba5IJwiX3GclG65xYKU2FEcFAoyJjCO6cQyLQWs9mtERKbriz00i2JtPPK0")
 // const stripe = "sk_test_51MSB2wIlCEHks7DgwfqyPzQNTkKjQiu40vmGeMLba5IJwiX3GclG65xYKU2FEcFAoyJjCO6cQyLQWs9mtERKbriz00i2JtPPK0"
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async (event) => {
 
