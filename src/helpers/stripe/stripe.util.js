85126de70ca043c2c605e3c43248e5a9347386ac
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-01-24 11:53:43 +0800

    move keys to .env

--- src/helpers/stripe/stripe.util.js
+++ src/helpers/stripe/stripe.util.js
@@ -3,8 +3,10 @@ import { loadStripe } from '@stripe/stripe-js'
 
 
 
+// working
 // export const stripePromise = loadStripe("pk_test_51MSB2wIlCEHks7DgasuuHAllQvZd4jicmn6wWE2y7PaMEKfZmA8EHRMYL0w7nwiQ3XX45OKlICGcX4VYbENIpHCp00hdOnSYPY");
 
 
-// export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
-export const stripePromise = loadStripe("pk_test_51MSB2wIlCEHks7DgasuuHAllQvZd4jicmn6wWE2y7PaMEKfZmA8EHRMYL0w7nwiQ3XX45OKlICGcX4VYbENIpHCp00hdOnSYPY");
+
+// test
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
