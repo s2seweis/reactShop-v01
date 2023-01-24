330911dfe26a93e901ceb1d9104fe89c3344387f
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-01-24 14:44:16 +0800

    add payment result

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -18,34 +18,50 @@ const PaymentForm = () => {
     e.preventDefault();
 
 
-if(!stripe || !elements) {
-  return;
-}
+    if (!stripe || !elements) {
+      return;
+    }
 
 
-const response = await fetch('/.netlify/functions/create-payment-intent', {
-// const response = await fetch('../../../netflify/functions/create-payment-intent', {
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      // const response = await fetch('../../../netflify/functions/create-payment-intent', {
 
-method: 'post',
+      method: 'post',
 
-headers: {
+      headers: {
 
-  'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
 
-},
+      },
 
-body: JSON.stringify( { amount: 1000} )
+      body: JSON.stringify({ amount: 1000 })
 
-}).then(res => res.json());
-
-
-// console.log(response);
-// console.log(response);
+    }).then(res => res.json());
 
 
-const {paymentIntent: {client_secret}} = response;
+    // console.log(response);
 
-console.log(client_secret);
+
+    const { paymentIntent: { client_secret } } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Basti Weiss'
+        }
+      }
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful');
+      }
+    }
 
 
   };
@@ -53,7 +69,7 @@ console.log(client_secret);
   return (
 
     <PaymentFormContainer>
-      <FormContainer onSubmit= {paymentHandler} >
+      <FormContainer onSubmit={paymentHandler} >
 
         <h2>Credit Card Payment:</h2>
 
