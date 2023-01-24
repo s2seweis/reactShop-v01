cb02bdb93813d722de6581e4d484e12eb6c5c366
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-01-24 12:36:49 +0800

    file changes dotenv

--- src/helpers/stripe/stripe.util.js
+++ src/helpers/stripe/stripe.util.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
+
+
+
+
+
 import { loadStripe } from '@stripe/stripe-js'
 
 
 
 
+
+
 // working
-// export const stripePromise = loadStripe("pk_test_51MSB2wIlCEHks7DgasuuHAllQvZd4jicmn6wWE2y7PaMEKfZmA8EHRMYL0w7nwiQ3XX45OKlICGcX4VYbENIpHCp00hdOnSYPY");
+export const stripePromise = loadStripe("pk_test_51MSB2wIlCEHks7DgasuuHAllQvZd4jicmn6wWE2y7PaMEKfZmA8EHRMYL0w7nwiQ3XX45OKlICGcX4VYbENIpHCp00hdOnSYPY");
 
 
 
-// test
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+// test - working too
+// export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
